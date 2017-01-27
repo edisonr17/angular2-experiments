1dc5916e4207a3b582113fb12fdf6cbbd0865d2f
Edison <edison.rave1517@gmail.com>
2017-01-27 09:38:13 -0500

    Plantilla de sisglo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, JsonpModule, RequestOptions } from '@angular/http';
+
+//Se utiliza para unas pruebas con firebase
 import { AngularFireModule } from 'angularfire2';
 
+//Componente Principal
 import { AppComponent } from './app.component';
+
+//Ejemplo de componente con firebase
 import { BodyPartsComponent } from './body-parts/body-parts.component';
+
+//Importamos modulo de rutas para la SPA
 import {routing} from './app.routing';
 
+//Ejemplo de la librería ng2-bootstrap usando un timepicker
 import { TimepickerModule } from 'ng2-bootstrap/timepicker';
-import { SgloHeaderComponentComponent } from './components/mainComponents/sglo-header-component/sglo-header-component.component';
-import { GridComponentComponent } from './components/commonsComponents/grid-component/grid-component.component';
 
+//Cargamos componentes de la plantilla principal de la aplicación
+import { SgloHeaderComponentComponent } from './components/mainComponents/sglo-header-component/sglo-header-component.component';
 import { SgloHomeComponentComponent } from './components/mainComponents/sglo-home-component/sglo-home-component.component';
-import { TranslateModule } from "ng2-translate";
 import { AsideComponentComponent } from './components/mainComponents/aside-component/aside-component.component';
 
 
+//Componente no necesario aún para generar un grid
+import { GridComponentComponent } from './components/commonsComponents/grid-component/grid-component.component';
 
- 
+//Módulo para el manejo de internacionalización
+import { TranslateModule } from "ng2-translate";
+
 
 
 
